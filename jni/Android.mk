e434f5474ec516f79d81c28abe31afe063c1eb4d
Subrat Panda <subrat.panda@flipkart.com>
2015-06-09 18:31:09 +0530

    Adding eclipse project files

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := MainActivity
 LOCAL_SRC_FILES := MainActivity.cpp
+LOCAL_SHARED_LIBRARIES := libliquidfun_jni
 
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-add-path,$(LIQUIDFUN_SRC_PATH)/..)
+$(call import-module,Box2D/swig/jni)
---
+++ jni/Application.mk
@@ -0,0 +1,10 @@
+APP_ABI:=x86
+APP_PLATFORM:=android-17
+APP_STL:=gnustl_static
+# APP_STL:=stlport_static
+APP_CFLAGS:=-DSWIG=1 -fexceptions -DLIQUIDFUN_EXTERNAL_LANGUAGE_API
+APP_PROJECT_PATH := $(abspath $(call my-dir)/..)
+LIQUIDFUN_SRC_PATH:=$(APP_PROJECT_PATH)/liquidfun-1.1.0/liquidfun/Box2D
+NDK_PROJECT_PATH:=$(APP_PROJECT_PATH)/android-ndk-r10e
+NDK_MODULE_PATH+=$(abspath $(NDK_PROJECT_PATH))
+SWIG_BIN:=/usr/local/bin/swig
